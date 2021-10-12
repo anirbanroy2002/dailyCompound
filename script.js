2e940d361d361a278daf35c2222eb1a6b1016a93
anirbanroy2002 <anirbanroy2002@gmail.com>
2021-10-12 19:08:49 +0530

    Bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let calculateButton = document.getElementById("calculate");
 calculateButton.addEventListener("click", calculate);
 function calculate()
     {
-    let initialInvestment = document.getElementById("amount").value;
+    let initialInvestment = parseInt(document.getElementById("amount").value);
     let rate = document.getElementById("rate").value;
     let months = document.getElementById("months").value;
     let result = document.getElementById("output");
@@ -15,6 +15,7 @@ function calculate()
         {
             result.insertAdjacentHTML("beforeend", "<div class='day'> Day " + (30 * (i - 1) + j) + ": " + initialInvestment.toFixed(2) + "</div><br>");
             initialInvestment*=1+rate/100.0;
+            
         }
     }
     window.location.hash = "#output";
